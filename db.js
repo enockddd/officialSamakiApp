68ae1d92ad35d7479556cc2dab0d3e327c93a713
enockddd <68570975+enockddd@users.noreply.github.com>
2023-07-15 15:26:23 +0300

    Update db.js

--- db.js
+++ db.js
@@ -1,12 +1,27 @@
 const mysql = require('mysql');
 
-const pool  = mysql.createPool({
-  connectionLimit : 10,
-  host     : 'enockddd.mysql.pythonanywhere-services.com',
-  user     : 'enockddd',
-  password : 'tish1997!q',
-  database : 'aquartic',
-  
+const pool = mysql.createPool({
+  connectionLimit: 10,
+  host: 'enockddd.mysql.pythonanywhere-services.com',
+  user: 'enockddd',
+  password: 'tish1997!q',
+  database: 'aquartic',
+  idleTimeoutMillis: 30000, // Set the idle timeout to 30 seconds (adjust as needed)
 });
 
 module.exports = pool;
+
+
+
+// const mysql = require('mysql');
+
+// const pool  = mysql.createPool({
+//   connectionLimit : 10,
+//   host     : 'enockddd.mysql.pythonanywhere-services.com',
+//   user     : 'enockddd',
+//   password : 'tish1997!q',
+//   database : 'aquartic',
+  
+// });
+
+// module.exports = pool;

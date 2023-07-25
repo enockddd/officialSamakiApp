a3c0dd83c5a87b83a79e133840ca98637950c6bd
enockddd <68570975+enockddd@users.noreply.github.com>
2023-07-25 14:55:14 +0300

    Update db.js

--- db.js
+++ db.js
@@ -1,13 +1,14 @@
 const mysql = require('mysql');
 
 const pool  = mysql.createPool({
-  connectionLimit : 10,
-  host     : 'enockddd.mysql.pythonanywhere-services.com',
-  user     : 'enockddd',
-  password : 'tish1997!q',
-  database : 'enockddd$aquartic',
+  connectionLimit: 10,
+  socketPath: '/cloudsql/fiery-azimuth-393715:us-central1:tish1997',
+  user: 'tish1997',
+  password: 'tish1997!q',
+  database: 'aquartic',
   
 });
 
 module.exports = pool;
+
 
